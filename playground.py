2ad1c6038ac0eda7efe058761434e8d742125c18
Johann Schwabe <johann.schwabe@uzh.ch>
2022-05-27 18:36:40 +0200

    a day

--- playground.py
+++ playground.py
@@ -3,21 +3,23 @@ from time import time_ns
 
 from BiddingAllocator.BiddingABOwner import BiddingABOwner
 from BiddingAllocator.BiddingAllocator import BiddingAllocator
+from Simulator.Allocator import FCFSAllocator
 from Simulator.Coordinate import TimeCoordinate
 from Simulator.Environment import Environment
 from Simulator import Simulator, Tick
 from Simulator.IO.JSONS import build_json
 from Simulator.History import History
-
+from Simulator.Owner.ABOwner import ABOwner
 
 dimensions = TimeCoordinate(10, 1, 10, Tick(40))
 random.seed(3)
 environment = Environment(dimensions, [], [])
 environment.init_blocker_tree()
-allocator = BiddingAllocator()
+# allocator = BiddingAllocator()
+allocator = FCFSAllocator()
 owners = []
 for i in range(3):
-    owners.append(BiddingABOwner("Schnabeltier"+ str(i), "red", [random.randint(0,5) for _ in range(random.randint(2,4))], random.random()))
+    owners.append(ABOwner("Schnabeltier"+ str(i), "red", [random.randint(0,5) for _ in range(random.randint(2,4))]))
 # owners = [BiddingABOwner("Schnabeltier", "red", [1,1,1,2], 0.5), BiddingABOwner("Schnabeltier", "red", [1,1,3,3,3], 0.7)]
 
 history = History(dimensions, allocator, environment, owners)
