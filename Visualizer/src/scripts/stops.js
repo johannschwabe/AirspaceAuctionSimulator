3983851172b62ce45b0b3e0758872678a2a532c0
Joel Barmettler <joel.barmettler@uzh.ch>
2022-08-03 15:17:31 +0200

    Update Visualizer/src/scripts/stops.js
    Co-authored-by: thommann <34217413+thommann@users.noreply.github.com>

--- Visualizer/src/scripts/stops.js
+++ Visualizer/src/scripts/stops.js
@@ -29,7 +29,6 @@ export function validStops(raw_positions, ownertype) {
       if (condition.startsWith(">")) {
         res.min = parseInt(condition.substring(1)) + 1 - (res.start ? 2 : 0);
       } else if (condition.startsWith("<")) {
-        console.log(condition.substring(1));
         res.max = parseInt(condition.substring(1)) - 1 - (res.start ? 2 : 0);
       } else {
         res.max = parseInt(condition) - (res.start ? 2 : 0);
