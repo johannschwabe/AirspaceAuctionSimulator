9650b6e303ec14ea3778f9de3d5b0b44883168c1
thommann <thomasrolf.mannhart@uzh.ch>
2022-08-29 09:15:49 +0200

    backend: undo rename

--- Demos/FCFS/__init__.py
+++ Demos/FCFS/__init__.py
@@ -1,3 +1,4 @@
 from .Allocator.FCFSAllocator import FCFSAllocator
 from .Owners.FCFSPathOwner import FCFSPathOwner
 from .Owners.FCFSSpaceOwner import FCFSSpaceOwner
+from .PaymentRule.FCFSPaymentRule import FCFSPaymentRule
--- Demos/Priority/__init__.py
+++ Demos/Priority/__init__.py
@@ -1,3 +1,4 @@
 from .Allocator.PriorityAllocator import PriorityAllocator
 from .Owners.PriorityPathOwner import PriorityPathOwner
 from .Owners.PrioritySpaceOwner import PrioritySpaceOwner
+from .PaymentRule.PriorityPaymentRule import PriorityPaymentRule
