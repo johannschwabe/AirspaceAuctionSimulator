d5eb0ab9b6da787088e62d94fc77c842134786a8
Johann Schwabe <johann.schwabe@uzh.ch>
2022-09-02 15:15:14 +0200

    env tested by example

--- test/test_environment.py
+++ test/test_environment.py
@@ -60,8 +60,8 @@ class EnvironmentTest(unittest.TestCase):
         self.assertIn(hash(allocation.agent), self.env.agents)
         r_tree_whole_path = self.env.tree.intersection([0, 0, 0, 0, 20, 20, 20, 50])
         r_tree_intersection_path = self.env.tree.intersection([4, 4, 4, 16, 4, 4, 4, 16])
-        self.assertEqual(len(list(r_tree_whole_path)), 19)
-        self.assertEqual(len(list(r_tree_intersection_path)), 1)
+        self.assertEqual(len(list(r_tree_whole_path)), 20)
+        self.assertEqual(len(list(r_tree_intersection_path)), 2)
 
     def test_allocate_segments_for_agents_2(self):
         allocation = generate_space_allocation()
@@ -81,7 +81,7 @@ class EnvironmentTest(unittest.TestCase):
         sub_block = self.env.tree.intersection([5, 5, 5, 10, 7, 7, 7, 20])
         self.assertEqual(len(list(sub_block)), 1)
         whole = self.env.tree.intersection([0, 0, 0, 0, 100, 100, 100, 100])
-        self.assertEqual(len(list(whole)), 22)
+        self.assertEqual(len(list(whole)), 23)
 
     def test_register_agent(self):
         agi = generate_space_agent()
@@ -165,6 +165,115 @@ class EnvironmentTest(unittest.TestCase):
         is_blocked_forever_2 = self.env.is_blocked_forever(Coordinate4D(10, 10, 10, 1), 1)
         self.assertFalse(is_blocked_forever)
         self.assertTrue(is_blocked_forever_2)
+
+    def test_have_intersections_collision(self):
+        agi_1 = generate_path_agent()
+        agi_2 = generate_path_agent()
+        agi_3 = generate_path_agent()
+
+        self.env.agents = {
+            hash(agi_1): agi_1,
+            hash(agi_2): agi_2,
+            hash(agi_3): agi_3,
+        }
+
+        segment = generate_path_segment(Coordinate4D(3, 3, 3, 3))
+        segment2 = generate_path_segment(Coordinate4D(6, 6, 6, 20))
+        segment2.index = 1
+        agi_1.add_allocated_segment(segment)
+        agi_1.add_allocated_segment(segment2)
+        res = self.env.have_intersections_collision(Coordinate4D(3, 3, 5, 6),
+                                                    agi_2,
+                                                    [hash(agi_1), hash(agi_3)], [hash(agi_3)])
+
+        self.assertTrue(res)
+        res2 = self.env.have_intersections_collision(Coordinate4D(3, 3, 5, 6),
+                                                     agi_2,
+                                                     [hash(agi_3), hash(agi_1)], [hash(agi_1)])
+        self.assertFalse(res2)
+
+    def test_is_blocked_by_agent(self):
+        agi = generate_path_agent()
+        agi_2 = generate_path_agent()
+        path_segment = generate_path_segment(Coordinate4D(3, 3, 3, 3))
+        self.env.allocate_path_segment_for_agent(agi, path_segment)
+        self.env.add_agent(agi)
+        self.assertTrue(self.env.is_blocked_by_agent(Coordinate4D(3, 3, 5, 6), agi_2))
+        self.assertTrue(self.env.is_blocked_by_agent(Coordinate4D(4, 3, 5, 6), agi_2))
+        self.assertFalse(self.env.is_blocked_by_agent(Coordinate4D(5, 3, 5, 6), agi_2))
+        agi.near_radius = 4
+        self.env.max_near_radius = 4
+        self.assertTrue(self.env.is_blocked_by_agent(Coordinate4D(5, 3, 5, 6), agi_2))
+
+    def test_is_space_blocked(self):
+        blocky = StaticBlocker(Coordinate3D(3, 3, 3), Coordinate3D(10, 10, 10))
+        blocky.id = 3
+        self.env.blocker_dict[blocky.id] = blocky
+        blocky.add_to_tree(self.env.blocker_tree, Coordinate4D(0, 0, 0, 100))
+        is_blocked = self.env.is_space_blocked(Coordinate4D(4, 4, 4, 4), Coordinate4D(15, 15, 15, 15))
+        self.assertTrue(is_blocked)
+        is_blocked_2 = self.env.is_space_blocked(Coordinate4D(14, 14, 14, 14), Coordinate4D(15, 15, 15, 15))
+        self.assertFalse(is_blocked_2)
+
+    def test_is_valid_for_allocation(self):
+        blocky = StaticBlocker(Coordinate3D(3, 3, 3), Coordinate3D(7, 7, 7))
+        blocky.id = 3
+        self.env.blocker_dict[blocky.id] = blocky
+        blocky.add_to_tree(self.env.blocker_tree, Coordinate4D(0, 0, 0, 100))
+        agi = generate_path_agent()
+        path_segment = generate_path_segment(Coordinate4D(11, 11, 11, 3))
+        self.env.allocate_path_segment_for_agent(agi, path_segment)
+        self.env.add_agent(agi)
+
+        agi_2 = generate_path_agent()
+        self.assertFalse(self.env.is_valid_for_allocation(Coordinate4D(4, 4, 4, 4), agi_2))
+        self.assertFalse(self.env.is_valid_for_allocation(Coordinate4D(11, 11, 13, 5), agi_2))
+        self.assertTrue(self.env.is_valid_for_allocation(Coordinate4D(11, 11, 14, 25), agi_2))
+
+    def test_other_agents_in_space(self):
+        agi = generate_path_agent()
+        path_segment = generate_path_segment(Coordinate4D(11, 11, 11, 3))
+        self.env.allocate_path_segment_for_agent(agi, path_segment)
+        self.env.add_agent(agi)
+
+        agi_2 = generate_space_agent()
+        other_agents = self.env.other_agents_in_space(Coordinate4D(9, 9, 9, 1), Coordinate4D(12, 12, 12, 5), agi_2)
+        self.assertEqual(1, len(other_agents))
+        self.assertIn(agi, other_agents)
+        other_agents_2 = self.env.other_agents_in_space(Coordinate4D(9, 9, 9, 52), Coordinate4D(12, 12, 12, 80), agi_2)
+        self.assertEqual(0, len(other_agents_2))
+
+    def test_new_clear(self):
+        blocky = StaticBlocker(Coordinate3D(3, 3, 3), Coordinate3D(7, 7, 7))
+        blocky.id = 3
+        self.env.blocker_dict[blocky.id] = blocky
+        blocky.add_to_tree(self.env.blocker_tree, Coordinate4D(0, 0, 0, 100))
+
+        agi = generate_path_agent()
+        path_segment = generate_path_segment(Coordinate4D(11, 11, 11, 3))
+        self.env.allocate_path_segment_for_agent(agi, path_segment)
+        self.env.add_agent(agi)
+
+        new_clear = self.env.new_clear()
+        agi_2 = generate_path_agent()
+        self.assertTrue(new_clear.is_blocked(Coordinate4D(4, 4, 4, 4), agi_2))
+        self.assertFalse(new_clear.is_blocked_by_agent(Coordinate4D(11, 11, 11, 3), agi_2))
+
+    def test_clone(self):
+        blocky = StaticBlocker(Coordinate3D(3, 3, 3), Coordinate3D(7, 7, 7))
+        blocky.id = 3
+        self.env.blocker_dict[blocky.id] = blocky
+        blocky.add_to_tree(self.env.blocker_tree, Coordinate4D(0, 0, 0, 100))
+
+        agi = generate_path_agent()
+        path_segment = generate_path_segment(Coordinate4D(11, 11, 11, 3))
+        self.env.allocate_path_segment_for_agent(agi, path_segment)
+        self.env.add_agent(agi)
+
+        new_clear = self.env.clone()
+        agi_2 = generate_path_agent()
+        self.assertTrue(new_clear.is_blocked(Coordinate4D(4, 4, 4, 4), agi_2))
+        self.assertTrue(new_clear.is_blocked_by_agent(Coordinate4D(11, 11, 11, 3), agi_2))
 
 
 if __name__ == '__main__':
