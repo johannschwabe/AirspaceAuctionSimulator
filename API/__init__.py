b617ed0234e79cf519650869b5da7f4ef3618ec2
thommann <thomasrolf.mannhart@uzh.ch>
2022-09-07 16:03:22 +0200

    merge dev

--- API/__init__.py
+++ API/__init__.py
@@ -1,9 +1,9 @@
 # API
-from .API import APIWorldCoordinates, app, APISimulationConfig, build_json
-from .Types import APIWorldCoordinates
+from .API import app, APISimulationConfig, build_json
 from .Area import Area
 # Generator
 from .Generator.EnvironmentGen import EnvironmentGen
 from .Generator.MapTile import MapTile
 # Runners
 from .Runners import run_from_config
+from .Types import APIWorldCoordinates
