6ebcddce9c688bbc2a14bfe27b310a4766ebc1ba
thommann <thomasrolf.mannhart@uzh.ch>
2022-09-07 16:41:25 +0200

    backend: PR fixes

--- API/__init__.py
+++ API/__init__.py
@@ -1,9 +1,6 @@
-# API
 from .API import app, APISimulationConfig, build_json
 from .Area import Area
-# Generator
 from .Generator.EnvironmentGen import EnvironmentGen
 from .Generator.MapTile import MapTile
-# Runners
 from .Runners import run_from_config
 from .Types import APIWorldCoordinates
