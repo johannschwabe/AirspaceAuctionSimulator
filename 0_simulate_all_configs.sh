dd1cb2479eebb8ceb401fe65efca575c8af7d05c
joelbarmettlerUZH <joel.barmettler@uzh.ch>
2022-10-18 21:44:36 +0200

    new config

--- 0_simulate_all_configs.sh
+++
@@ -1,6 +0,0 @@
-for f in $(ls "$(pwd)/Prefabs/configs/"); do
- NEWFILENAME=$(echo $f | sed -e 's/\-config.json//g');
- NEWFILENAMEEXT="$NEWFILENAME-output.json";
- echo "Executing -> $f - Will save as -> $NEWFILENAMEEXT";
- python -u CLI.py --prefab "$NEWFILENAME" --save-simulation "$(pwd)/Prefabs/outputs/$NEWFILENAMEEXT" --skip-summary --skip-save-config --simulate
-done
